94a93483494c39f32a93ee538c25b98dec1d0fb9
malekyla0372 <gusev.matyusha@gmail.com>
2026-04-22 22:08:46 +0300

    6.2 Интеграция Pytest и Playwright. Практика написания автотестов

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,7 +1,6 @@
 from playwright.sync_api import sync_playwright, expect
 
 def test_empty_courses_list():
-    from playwright.sync_api import sync_playwright, expect
 
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
