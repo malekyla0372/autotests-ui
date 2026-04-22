6f2c64593dc266542f466a4be9139aa1768355cd
malekyla0372 <gusev.matyusha@gmail.com>
2026-04-22 16:30:25 +0300

    Registration local storage script

--- playwright_registration_local_storage.py
+++ playwright_registration_local_storage.py
@@ -21,8 +21,6 @@ with sync_playwright() as playwright:
 
     context.storage_state(path='browser-state.json')
 
-from playwright.sync_api import sync_playwright
-
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
