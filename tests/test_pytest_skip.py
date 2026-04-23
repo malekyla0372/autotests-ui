23bb14ef1013fd1924052296c60caefff4590522
malekyla0372 <gusev.matyusha@gmail.com>
2026-04-23 18:42:16 +0300

    Pytest skip, skipif, xfail

--- tests/test_pytest_skip.py
+++ tests/test_pytest_skip.py
@@ -1,10 +1,10 @@
 import pytest
 
-@pytest.mark.skip(reason='Feature under development')
+@pytest.mark.skip(reason='Feature not implemented yet')
 def test_future_development():
     ...
 
-@pytest.mark.skip(reason='Feature under development')
+@pytest.mark.skip(reason='Feature not implemented yet')
 class TestFutureDevelopment:
     def test_future_development_1(self):
         ...
