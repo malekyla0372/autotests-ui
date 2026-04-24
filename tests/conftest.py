4a4d322a0af9ae8a8698a7e7ad0358e972041fa2
malekyla0372 <gusev.matyusha@gmail.com>
2026-04-25 00:21:14 +0300

    6.5 Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,46 +4,46 @@ from playwright.sync_api import sync_playwright, Page, Playwright
 
 @pytest.fixture
 def chromium_page(playwright) -> Page:
-        browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False)
         yield browser.new_page()
         browser.close()
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright) -> Page:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-        email_input = page.get_by_test_id("registration-form-email-input").locator('input')
-        email_input.fill("user@gmail.com")
+    email_input = page.get_by_test_id("registration-form-email-input").locator('input')
+    email_input.fill("user@gmail.com")
 
-        username_input = page.get_by_test_id("registration-form-username-input").locator('input')
-        username_input.fill("username")
+    username_input = page.get_by_test_id("registration-form-username-input").locator('input')
+    username_input.fill("username")
 
-        password_input = page.get_by_test_id("registration-form-password-input").locator('input')
-        password_input.fill("password")
+    password_input = page.get_by_test_id("registration-form-password-input").locator('input')
+    password_input.fill("password")
 
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-        page.wait_for_url("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+    page.wait_for_url("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
 
-        context.storage_state(path='browser-state.json')
+    context.storage_state(path='browser-state.json')
 
-        yield
+    yield
 
-        browser.close()
+    browser.close()
 
 
 @pytest.fixture(scope="function")
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='browser-state.json')
-        page = context.new_page()
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
 
-        yield page
+    yield page
 
-        context.close()
-        browser.close()
+    context.close()
+    browser.close()
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -5,16 +5,16 @@ import pytest
 @pytest.mark.regression
 def test_empty_courses_list(chromium_page_with_state: Page):
 
-        chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        courses_visible = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses_visible).to_be_visible()
+    courses_visible = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_visible).to_be_visible()
 
-        folder_visible = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-        expect(folder_visible).to_be_visible()
+    folder_visible = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(folder_visible).to_be_visible()
 
-        no_results_visible = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-        expect(no_results_visible).to_be_visible()
+    no_results_visible = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(no_results_visible).to_be_visible()
 
-        description_visible = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-        expect(description_visible).to_be_visible()
+    description_visible = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(description_visible).to_be_visible()
