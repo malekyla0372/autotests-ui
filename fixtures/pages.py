73f77a4453295318df87282ec6430814be6a3c75
malekyla0372 <gusev.matyusha@gmail.com>
2026-05-05 20:59:11 +0300

    PageObject

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,10 +1,8 @@
-from lib2to3.pgen2 import driver
-
 import pytest
 from playwright.sync_api import Page
 
 from fixtures.browsers import chromium_page
 from pages.login_page import LoginPage
 
-def login_page(chromium_page: Page) -> LoginPage: # зачем рисовать эти стрелочки и что значат двоеточие и параметр такой
-    return LoginPage(page=chromium_page()) # я так понимаю что тут задаю параметры странице типо какую страницу жду
+def login_page(chromium_page: Page) -> LoginPage:
+    return LoginPage(page=chromium_page)
