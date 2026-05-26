2c7a65b333b5b0fb91b35bd3d5b4363092beb149
malekyla0372 <gusev.matyusha@gmail.com>
2026-05-26 21:12:58 +0300

    PageComponent

--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -1,17 +1,17 @@
-from playwright.sync_api import Page, expect
-
 from typing import Pattern
+
+from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
 
 
 class SidebarListItemComponent(BaseComponent):
-    def __init__(self, page: Page):
+    def __init__(self, page: Page, identifier: str):
         super().__init__(page)
 
-        self.icon = page.get_by_test_id("courses-drawer-list-item-icon")
-        self.title = page.get_by_test_id("courses-drawer-list-item-title-text")
-        self.button = page.get_by_test_id("courses-drawer-list-item-button")
+        self.icon = page.get_by_test_id(f'{identifier}-drawer-list-item-icon')
+        self.title = page.get_by_test_id(f'{identifier}-drawer-list-item-title-text')
+        self.button = page.get_by_test_id(f'{identifier}-drawer-list-item-button')
 
     def check_visible(self, title: str):
         expect(self.icon).to_be_visible()
