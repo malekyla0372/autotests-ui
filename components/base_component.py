ef4dd5b62290279c92eee59d88ceb40f8fd972d3
malekyla0372 <gusev.matyusha@gmail.com>
2026-08-04 17:13:38 +0300

    9.5 Логирование взаимодействий со страницей. Практика работы с логированием

--- components/base_component.py
+++ components/base_component.py
@@ -1,5 +1,9 @@
 from typing import Pattern
 from playwright.sync_api import Page, expect
+from tools.logger import get_logger
+import allure
+
+logger = get_logger("BASE_COMPONENT")
 
 
 class BaseComponent:
@@ -7,4 +11,8 @@ class BaseComponent:
         self.page = page
 
     def check_current_url(self, expected_url: Pattern[str]):
-        expect(self.page).to_have_url(expected_url)
+        step = f"Checking that current url matches pattern {expected_url}"
+
+        with allure.step(step):
+            logger.info(step)
+            expect(self.page).to_have_url(expected_url)
